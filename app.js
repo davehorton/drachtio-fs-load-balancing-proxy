c8578250c7840cd2bbb926a8be30010e94a4fa55
Dave Horton <daveh@beachdognet.com>
2016-06-01 19:19:59 -0400

    add blacklist feature

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ var app = drachtio() ;
 var Srf = require('drachtio-srf') ;
 var srf = new Srf(app) ;
 var config = require('./lib/config') ;
+var logger = app.logger = require('./lib/logger');
 var rangeCheck = require('range_check');
+var blacklist = require('./lib/blacklist') ;
 var _ = require('lodash') ;
 
 srf.connect(config.drachtioServer) ;
 
 srf.on('connect', function(err, hostport) {
-  console.log('connected to drachtio listening for SIP on %s', hostport) ;
+  logger.info('connected to drachtio listening for SIP on %s', hostport) ;
 }) ;
 
 
@@ -26,6 +28,14 @@ function checkSender(req, res, next) {
 if( _.isArray( config.authorizedSources ) && config.authorizedSources.length > 1 ) {
   srf.use(checkSender) ;
 }
+
+// optionally detect SIP scanners and blackhole them
+if( config.blacklist && config.blacklist.chain ) {
+  srf.use(blacklist({logger: logger, chain: config.blacklist.chain, realm: config.blacklist.chain})) ;
+}
+
+// Expose app
+exports = module.exports = app;
 
 //load routes
 require('./lib/proxy')(srf) ;
